503e7d996ab8d9c0e023ce4fd6f22b2ea056aab9
mgcodex1 <62226574+mgcodex1@users.noreply.github.com>
2020-03-15 22:15:06 -0400

    Add files via upload

---
+++ rock.py
@@ -0,0 +1,6 @@
+Python 2.7.12 (default, Oct  8 2019, 14:14:10) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> print('Hello World')
+Hello World
+>>> 
